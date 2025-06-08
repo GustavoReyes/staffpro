b4ca91c412d2b12068be9570e16d7f5c772d1c98
Ariani <arianicarolina@gmail.com>
2025-06-08 23:30:46 +0200

    aria 8/6 23:30

--- staff-pro-front-end/src/app/services/Employees/employees.service.ts
+++ staff-pro-front-end/src/app/services/Employees/employees.service.ts
@@ -1,14 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Employee } from '../../model/employee';
 
 @Injectable({ providedIn: 'root' })
 export class EmployeesService {
-  private apiUrl = 'http://localhost:3000/employees/findAll';
+  private apiUrl = 'http://localhost:3000/employees/';
 
   constructor(private http: HttpClient) {}
 
-  getEmployees(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+
+  registerEmployee(email: string, datos: any): Observable<boolean> {
+
+     const url:string =`${this.apiUrl}/alta/${email}`;
+     return this.http.post<boolean>(url,datos)
+     .pipe(map(()=>true),
+                   catchError(e=>of(false)));
   }
+
+
+  getEmployees(): Observable<Employee[]> {
+        const url:string =`${this.apiUrl} findAll`;
+    return this.http.get<Employee[]>(url);
+  }
+
+
+  employeesByDepartment(department_id: number): Observable<Employee[]> {
+ const url:string =`${this.apiUrl} findByDepart/${department_id}`;
+    return this.http.get<Employee[]>(url);
+  }
+employeeByEmail(email: string): Observable<Employee> {
+ const url:string =`${this.apiUrl} findByEmail/${email}`;
+    return this.http.get<Employee>(url);
+  }
+
+updateByEmail(email: string, datos:any): Observable<any> {
+  return this.http.patch(`/apiUrl/patch/${email}`, datos);
+  }
+ deleteByEmail(email: string): Observable<any> {
+    return this.http.delete(`/apiUrl/delete/${email}`);
+  }
+
 }
