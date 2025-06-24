bd4e654596f40bdd725d9e333137f3590bb8f471
Gustavo Reyes <gustavoabrito@gmail.com>
2025-06-24 20:07:13 +0200

    Adds Spanish locale support and refines payroll forms
    Sets application locale to Spanish, registering appropriate locale data for consistent date and number formatting. Enhances employee and payroll edit forms with improved input alignment, formatting, and additional IRPF field. Improves clarity of certain labels for better user experience.

--- staff-pro-front-end/src/main.ts
+++ staff-pro-front-end/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { MainComponent } from './app/components/main/main.component';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs);
 
 bootstrapApplication(MainComponent, appConfig)
   .catch((err) => console.error(err));
